b0a756610badeb2019639dc6abc484ba53bd3396
Dmitry Fedotov <dm.fe@yandex.ru>
2021-03-04 17:09:16 +0300

    Math and stack examples.

---
+++ math-operation/print-digit.asm
@@ -0,0 +1,47 @@
+
+section .data
+        digit db 0,10
+
+section .text
+        global _start
+
+_start:
+        mov rax, 7
+        call _printRAXDigit
+
+        mov rax, 6
+        mov rbx, 2
+        div rbx
+        call _printRAXDigit
+
+        mov rax, 1
+        add rax, 4
+        call _printRAXDigit
+
+        ; Stack example
+        push 4
+        push 8
+        push 3
+
+        pop rax
+        call _printRAXDigit
+        pop rax
+        call _printRAXDigit
+        pop rax
+        call _printRAXDigit
+
+        mov rax, 60
+        mov rdi, 0
+        syscall
+
+_printRAXDigit:
+
+        add rax, 48
+        mov [digit], al
+        mov rax, 1
+        mov rdi, 1
+        mov rsi, digit
+        mov rdx, 2
+        syscall
+        xor rdx, rdx
+        ret
